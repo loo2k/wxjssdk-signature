af215aeda66511831f961380ee323363bbefff64
luke <loo2km@gmail.com>
2015-03-31 15:25:26 +0800

    [*] 调整为修改版的官方 sdk

--- signature.php
+++ signature.php
@@ -1,13 +1,11 @@
 <?php
 
-require_once './wxjsSignature.class.php';
+require_once './jssdk.php';
 
 $url = $_GET['url'];
 
-$wxjsSignature = new wxjsSignature();
-$wxjsSignature->appid = '';
-$wxjsSignature->appsecret = '';
-$wxconfig = $wxjsSignature->config($url);
+$jssdk = new JSSDK('<% your appid %>', '<% your appsecret %>');
+$wxconfig = $jssdk->getSignPackage($url);
 
 header('Access-Control-Allow-Origin: *');
 echo json_encode($wxconfig);
